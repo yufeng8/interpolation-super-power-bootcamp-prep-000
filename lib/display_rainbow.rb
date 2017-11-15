54a42a39a69a116913c20570e1ceb7992cc2df15
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-15 21:50:14 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   s=""
-  for i in 0..colors.length()
+  for i in 0..colors.length
     s.concat("#{colors[i][0].uppercase}: #{color[i]}, ")
   end
   return s
