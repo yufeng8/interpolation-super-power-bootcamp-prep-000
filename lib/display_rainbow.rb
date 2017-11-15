0ce02ecb697cc08ba60b212ae3f96f73e0cab89d
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-15 22:10:27 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,7 +6,7 @@ def display_rainbow(colors)
     s1= colors[i][0].upcase
     s2= colors[lcolors.ength-1].upcase
     s = s.concat("#{s1}: #{colors[i]}, ")
-    s.concat("#{s2}: #{colors[colors.length-1]})
+    s.concat("#{s2}: #{colors[colors.length-1]}")
   end
   return s
 end
